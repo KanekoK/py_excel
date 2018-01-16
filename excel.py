455b0c24708e60640f2282e5893704d3354002b8
Kenya <b1r2d3q4@gmail.com>
2018-01-16 09:19:04 +0900

    .

--- excel.py
+++ excel.py
@@ -1,1 +1,12 @@
 import xlrd
+
+# Excel ファイル（ブック）を読み込み
+book = xlrd.open_workbook('data.xlsx')
+
+# ブック内のシート数を取得
+num_of_worksheets = book.nsheets
+print(num_of_worksheets)
+
+# 全シートの名前を取得
+sheet_names = book.sheet_names()
+print(sheet_names)
